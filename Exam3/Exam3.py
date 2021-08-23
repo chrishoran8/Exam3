04b3d3110bcb21b29eb479ce0305c8c04b8beaa4
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-23 20:25:33 +0100

    ready for mick to test

--- Exam3/Exam3.py
+++ Exam3/Exam3.py
@@ -12,32 +12,35 @@
 # \\192.168.130.21\chshare\regiNumber
 
 #####################################################
-#Section 1 20 Marks
+#Section 1 25 Marks
 
 
-#1) Create and Assign a type float variable called fltOne the value 10 (2)
+#1) Create and Assign a type float variable called fltOne the value 10 (3)
 fltOne = float(10)
 
-#2) Create and Assign a type float variable called fltTwo the value 20 (2)
+#2) Create and Assign a type float variable called fltTwo the value 20 (3)
 fltTwo = float(20)
 
 #3) Create and Assign a type float variable called fltThree with the product of fltTwo and fltOne(3)
 fltThree = float(fltOne * fltTwo)
 
-#4) Create and assign a variable called stringOne with the value "The product of fltOne and fltTwo = "(2)
+#4) Create and assign a variable called stringOne with the value "The product of fltOne and fltTwo = "(3)
 stringOne = "The product of fltOne and fltTwo = "
 
-#5) On the console, output stringOne and fltThree (in that order) (4)
+#5) On the console, output stringOne and fltThree (in that order) (3)
 print(stringOne,fltThree)
 
 #6) increment fltOne  (3)
 fltOne = fltOne + 1
 
-#7)  prompt the user to provide an input to fltTwo with the message "Please provide another number for fltTwo" (4)
+#7)  prompt the user to provide an input to fltTwo with the message "Please provide another number for fltTwo". Ensure a float is given (4)
 fltTwo = float(input("Please provide another number for fltTwo"))
 
+#8) on the console, output the product of fltOne and flotTwo with a suitable message (3) 
+print("The new value is", fltOne * fltTwo)
+
 #####################################################
-#Section 2 30 Marks
+#Section 2 35 Marks
 
 #8) take the input from fltTwo and apply a decision based on the number inputted . 
 #The decision should be based on if the user inputs a number below 100 
@@ -45,57 +48,57 @@ fltTwo = float(input("Please provide another number for fltTwo"))
 if fltTwo < 100:
     print("below 100")
 
-#9) take the product of fltTwo and fltOne.
+#9) take the difference between of fltOne and fltTwo (fltOne - fltTwo)
+#Using if, else and elif, output the following: 
 #If the product is below 0, output "below 0"
-#if the product is 0 or above and below 100, output "from 0 to 100"
-#if the product is above 100 output "above 100" (6)
-if fltOne * fltTwo < 0:
+#if the product is 0 output "value is zero"
+#if the product is above 0 output "above 0" (8)
+if fltOne - fltTwo < 0:
     print("below 0")
-elif fltOne * fltTwo >= 0 and fltOne * fltTwo < 100:
-    print("from 0 to 100")
+elif fltOne - fltTwo == 0:
+    print("value is zero")
 else:
-    print("above 100")
+    print("above 0")
 
 
 #10) create a list called listOfInts (4)
 listOfInts = []
 
 #11 part a) create a for loop to iterate through the above list and populate the list with 
-#{4,6,8,10,12,14,16,18,20,22} 
-#(6)
-for i in range(4,23,2):
-    print(i)
+#{4,6,8,10,12,14,16,18,20,22}. Output the list to the console with a suitable message(7)
+for i in range(4,23,2):    
     listOfInts.append(i)
+print("original list of ints",listOfInts)
 
 #11 part b) create a for loop to iterate through the above list and 
-#multiply each value by two assigning the new value to the respective 
-#index in the list. The list should now look like {8,12,16.....} (7)
+#multiply each value by three assigning the new value to the respective 
+#index in the list. The list should now look like {12,18,24.....} (8)
 counter = 0
 for i in listOfInts:    
-    listOfInts[counter] = i * 2
+    listOfInts[counter] = i * 3
     counter = counter + 1
 
-#11 part c )output listOfInts to the screen (2)
-print(listOfInts)
+#11 part c )output listOfInts to the screen with a suitable message (3)
+print("new list of ints",listOfInts)
 
 #####################################################
 #Section 3 20 marks
 
-#14) create a function which calculates an increase of a given percentage (10)
+#14) create a function which calculates a decrease of a given percentage (10 marks)
 # the function should be called calcPerc
 #the function should take a cost parameter and a percentage parameter
-#it should return the cost plus the percentage increas. 
-#For example if the paramenters are cost = 100 and percentage = 50, it should return 150. 
-#For another example, if the parameters are cost = 50 and percentage = 10, it should return 55.
+#it should return the cost less the percentage amount
+#For example if the paramenters are cost = 100 and percentage = 50, it should return 50. 
+#For another example, if the parameters are cost = 50 and percentage = 10, it should return 45.
 #The percentage value should assume 10% if no value is given
-#test the function here:
+
 def calcPerc(cost,percentage=10):
-    return cost + cost*(percentage/100)
+    return cost - cost*(percentage/100)
 print(calcPerc(50,10))
 
 
 #15) create a function called caseChanger which takes a string argument written all in lower case
-#and returns a string where any e found, it will convert it to a capital E (10)
+#It will output all letters in lowercase except e which it will conver to capital E (10 marks)
 #For example, caseChanger("hello") prints hEllo to the console
 
 def caseChanger(word):
@@ -108,32 +111,28 @@ caseChanger("hello")
 
 
 ##################################################### 
-#Section 4 30 marks
+#Section 4 20 marks
 
-#16)a) Create a list that represents a set of students. The tuple should contain the following
-#students: Clark,Cox,Horan,Rai,Smith,White,Cooper,Jones,,
-nonOrderedStudents= ["Clark","Cox","Horan","Rai","Smith","White","Cooper","Jones"]
-print(nonOrderedStudents)
-#b) create a Tuple with the students now ordered alphabetically
-students = ()
-students = nonOrderedStudents.sort()
-print(students)
+#16)a) Create a list that represents a set of students. The list should contain the following
+#students: Clark,Horan,Rai,White,Cooper,Jones,Cox,Smith (4 marks)
+students= ["Clark","Horan","Rai","White","Cooper","Jones","Cox","Smith"]
+
+#b) use a method to order the students so that they are in alphabetical order (3 marks)
+students.sort()
 
 
-#17) create a tuple that represents exam marks with the following data. 
+#17) create a tuple that represents exam marks with the following data. (4 marks)
 #These are the respective exam marks for the alphabetically ordered student list
 # 65,66,67,80,90,65,65,93
 scores = (65,66,67,80,90,65,65,93)
-print(scores)
+print("scores ",scores)
 
-#18) Dictionary question 1 
+#18) Dictionary question (9 marks)
 #create a dictionary which joins the student with their corresponding mark. 
-#There are a number of ways to achieve this. You will be awarded marks for 
-#the most efficient method
+#There is a manual way of doing this or a way involving using the previous tuple and list
+#you will be awarded highest marks for 
+#output the student with their corresponding mark
 studentMarks= {}
-for i in range(8):
+for i in range(8):    
     studentMarks[students[i]] = scores[i]
 print(studentMarks)
-
-
-
